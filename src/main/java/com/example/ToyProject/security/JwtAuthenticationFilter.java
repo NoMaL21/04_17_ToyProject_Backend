175290d576980a991f8771e464c80e75f96fdc6f
NoMaL <dntjd8509@gmail.com>
2023-04-17 16:33:23 +0900

    2. GPT 개선 여전히 안됨

--- src/main/java/com/example/ToyProject/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/ToyProject/security/JwtAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package com.example.ToyProject.security;
 
+import org.springframework.http.MediaType;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -18,46 +21,54 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
-	@Autowired
-	private TokenProvider tokenProvider;
-	
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, 
-			FilterChain filterChain) throws ServletException, IOException {
-		
-		try {
-			String token = parseBearerToken(request);
-			log.info("Filter is running...");
-			
-			if ( token != null &&!token.equalsIgnoreCase("null")) {
-				String userId = tokenProvider.validateAndGetuserId(token);
-				log.info("Authenticated user Id : " + userId);
-				AbstractAuthenticationToken authentication = 
-						new UsernamePasswordAuthenticationToken(userId, null, AuthorityUtils.NO_AUTHORITIES);
-				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-				securityContext.setAuthentication(authentication);
-				SecurityContextHolder.setContext(securityContext);
-			}
-		}catch(Exception ex) {
-			logger.error("Could not set user authentication in security context",ex);
-		}
-			
-		filterChain.doFilter(request, response);
-	}
-	
-	private String parseBearerToken(HttpServletRequest request) {
-		String bearerToken = request.getHeader("Authorization");
-		
-		if(StringUtils.hasText(bearerToken)&&bearerToken.startsWith("Bearer ")) {
-			return bearerToken.substring(7);
-		}
-		return null;
-	}
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-}	
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = parseBearerToken(request);
+            log.info("Filter is running...");
+
+            if (token != null && !token.equalsIgnoreCase("null")) {
+                String userId = tokenProvider.validateAndGetuserId(token);
+                log.info("Authenticated user Id : " + userId);
+                AbstractAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userId, null, AuthorityUtils.NO_AUTHORITIES);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                securityContext.setAuthentication(authentication);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        } catch (Exception ex) {
+            logger.error("Could not set user authentication in security context", ex);
+
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            Map<String, Object> data = new HashMap<>();
+            data.put("status", HttpServletResponse.SC_FORBIDDEN);
+            data.put("message", ex.getMessage());
+            objectMapper.writeValue(response.getOutputStream(), data);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseBearerToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
